b7bd7eb200a097c4fb6140f4721bdbe04ce772ce
itchyny <itchyny@hatena.ne.jp>
2017-10-12 00:51:12 +0900

    avoid loadavg pointer conversion

--- loadavg/loadavg_unix.go
+++ loadavg/loadavg_unix.go
@@ -4,21 +4,20 @@ package loadavg
 
 import (
 	"errors"
-	"unsafe"
 )
 
 // #include <stdlib.h>
 import "C"
 
 func get() (*Loadavg, error) {
-	var loadavgs [3]float64
-	ret := C.getloadavg((*C.double)(unsafe.Pointer(&loadavgs)), 3)
+	var loadavgs [3]C.double
+	ret := C.getloadavg(&loadavgs[0], 3)
 	if ret != 3 {
 		return nil, errors.New("failed to get loadavg")
 	}
 	return &Loadavg{
-		Loadavg1:  loadavgs[0],
-		Loadavg5:  loadavgs[1],
-		Loadavg15: loadavgs[2],
+		Loadavg1:  float64(loadavgs[0]),
+		Loadavg5:  float64(loadavgs[1]),
+		Loadavg15: float64(loadavgs[2]),
 	}, nil
 }
