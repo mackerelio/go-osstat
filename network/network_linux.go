69b6a46fd9e16b6762343cd488bd7d0dd98ad144
itchyny <itchyny@hatena.ne.jp>
2017-10-14 15:10:24 +0900

    implement network statistic for linux

---
+++ network/network_linux.go
@@ -0,0 +1,62 @@
+// +build linux
+
+package network
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Get network statistics
+func Get() ([]Network, error) {
+	file, err := os.Open("/proc/net/dev")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	networks, err := collectNetworkStats(file)
+	if err != nil {
+		return nil, err
+	}
+	return networks, nil
+}
+
+// Network represents network statistics for linux
+type Network struct {
+	Name             string
+	RxBytes, TxBytes uint64
+}
+
+func collectNetworkStats(out io.Reader) ([]Network, error) {
+	scanner := bufio.NewScanner(out)
+	var networks []Network
+	rxBytesIdx, txBytesIdx := 1, 9
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		if len(fields) < 17 || len(fields) > 0 && !strings.HasSuffix(fields[0], ":") {
+			continue
+		}
+		name := fields[0][:len(fields[0])-1]
+		if name == "lo" {
+			continue
+		}
+		rxBytes, err := strconv.ParseUint(fields[rxBytesIdx], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse rxBytes of %s", name)
+		}
+		txBytes, err := strconv.ParseUint(fields[txBytesIdx], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse txBytes of %s", name)
+		}
+		networks = append(networks, Network{Name: name, RxBytes: rxBytes, TxBytes: txBytes})
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return networks, nil
+}
---
+++ network/network_linux_test.go
@@ -0,0 +1,41 @@
+// +build linux
+
+package network
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestGetnetwork(t *testing.T) {
+	networks, err := Get()
+	if err != nil {
+		t.Errorf("error should be nil but got: %v", err)
+	}
+	for _, network := range networks {
+		if network.Name == "en0" && network.RxBytes <= 0 {
+			t.Errorf("invalid network value: %+v", network)
+		}
+	}
+}
+
+func TestCollectNetworkStats(t *testing.T) {
+	got, err := collectNetworkStats(strings.NewReader(
+		`Inter-|   Receive                                                |  Transmit
+ face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
+ wlan0: 1188035151  850857    0    0    0     0          0         0 49774221  428282    0    0    0     0       0          0
+    lo: 1292817    9913    0    0    0     0          0         0  1292817    9913    0    0    0     0       0          0
+  eth0: 26054426   73542    0    0    0     0          0         0 12352148   58473    0    0    0     0       0          0
+`))
+	if err != nil {
+		t.Errorf("error should be nil but got: %v", err)
+	}
+	expected := []Network{
+		{"wlan0", 1188035151, 49774221},
+		{"eth0", 26054426, 12352148},
+	}
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("invalid network value: %+v (expected: %+v)", got, expected)
+	}
+}
