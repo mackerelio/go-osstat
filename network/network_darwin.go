b25da8b719974baa276ec0c3ec88d248f426d5e9
itchyny <itchyny@hatena.ne.jp>
2017-10-14 14:45:39 +0900

    implement network statistic for darwin

---
+++ network/network_darwin.go
@@ -0,0 +1,72 @@
+// +build darwin
+
+package network
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+// Get network statistics
+func Get() ([]Network, error) {
+	cmd := exec.Command("netstat", "-bni")
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+	networks, err := collectNetworkStats(out)
+	if err != nil {
+		return nil, err
+	}
+	if err := cmd.Wait(); err != nil {
+		return nil, err
+	}
+	return networks, nil
+}
+
+// Network represents network statistics for darwin
+type Network struct {
+	Name             string
+	RxBytes, TxBytes uint64
+}
+
+func collectNetworkStats(out io.Reader) ([]Network, error) {
+	scanner := bufio.NewScanner(out)
+	if !scanner.Scan() { // skip the first line
+		return nil, fmt.Errorf("failed to scan output of netstat")
+	}
+
+	var networks []Network
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		name := strings.TrimSuffix(fields[0], "*")
+		if strings.HasPrefix(name, "lo") || !strings.HasPrefix(fields[2], "<Link#") {
+			continue
+		}
+		rxBytesIdx, txBytesIdx := 6, 9
+		if len(fields) == 10 {
+			rxBytesIdx, txBytesIdx = 5, 8
+		}
+		rxBytes, err := strconv.ParseUint(fields[rxBytesIdx], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse Ibytes of %s", name)
+		}
+		txBytes, err := strconv.ParseUint(fields[txBytesIdx], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse Obytes of %s", name)
+		}
+		networks = append(networks, Network{Name: name, RxBytes: rxBytes, TxBytes: txBytes})
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scan error for netstat: %s", err)
+	}
+
+	return networks, nil
+}
---
+++ network/network_darwin_test.go
@@ -0,0 +1,51 @@
+// +build darwin
+
+package network
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestGetNetwork(t *testing.T) {
+	networks, err := Get()
+	if err != nil {
+		t.Errorf("error should be nil but got: %v", err)
+	}
+	for _, network := range networks {
+		if network.Name == "en0" && network.RxBytes <= 0 {
+			t.Errorf("invalid network value: %+v", network)
+		}
+	}
+}
+
+func TestCollectNetworkStats(t *testing.T) {
+	got, err := collectNetworkStats(strings.NewReader(
+		`Name  Mtu   Network       Address            Ipkts Ierrs     Ibytes    Opkts Oerrs     Obytes  Coll
+lo0   16384 <Link#1>                       1758927     0  601295906  1758927     0  601295906     0
+lo0   16384 127           127.0.0.1        1758927     -  601295906  1758927     -  601295906     -
+lo0   16384 ::1/128     ::1                1758927     -  601295906  1758927     -  601295906     -
+lo0   16384 fe80::1%lo0 fe80:1::1          1758927     -  601295906  1758927     -  601295906     -
+gif0* 1280  <Link#2>                             0     0          0        0     0          0     0
+stf0* 1280  <Link#3>                             0     0          0        0     0          0     0
+en0   1500  <Link#4>    a4:5e:60:aa:aa:aa 23300388     0 18096041919 17727990     0 8602191509     0
+en0   1500  fe80::222:a fe80:4::222:aaaa: 23300388     - 18096041919 17727990     - 8602191509     -
+en0   1500  192.168.105   192.168.105.102 23300388     - 18096041919 17727990     - 8602191509     -
+en1   1500  <Link#5>    4a:00:00:aa:aa:aa        0     0          0        0     0          0     0
+en2   1500  <Link#6>    4a:00:00:aa:aa:aa        0     0          0        0     0          0     0
+`))
+	if err != nil {
+		t.Errorf("error should be nil but got: %v", err)
+	}
+	expected := []Network{
+		{"gif0", 0, 0},
+		{"stf0", 0, 0},
+		{"en0", 18096041919, 8602191509},
+		{"en1", 0, 0},
+		{"en2", 0, 0},
+	}
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("invalid network value: %+v (expected: %+v)", got, expected)
+	}
+}
