d3eb75027e77a1102c1b120ed7f77081e122c2db
itchyny <itchyny@hatena.ne.jp>
2017-10-15 20:50:33 +0900

    add memory statistics for Windows

---
+++ memory/memory_windows_test.go
@@ -0,0 +1,17 @@
+// +build windows
+
+package memory
+
+import (
+	"testing"
+)
+
+func TestGetMemory(t *testing.T) {
+	memory, err := Get()
+	if err != nil {
+		t.Fatalf("error should be nil but got: %v", err)
+	}
+	if memory.Used <= 0 || memory.Total <= 0 || memory.Free <= 0 {
+		t.Errorf("invalid memory value: %+v", memory)
+	}
+}
