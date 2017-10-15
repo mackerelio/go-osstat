cf1e19533c0f2b2faa1cdd07307740b9ebf38f07
itchyny <itchyny@hatena.ne.jp>
2017-10-15 11:25:27 +0900

    support loadavg for nocgo on linux

---
+++ loadavg/loadavg_unix_nocgo.go
@@ -0,0 +1,27 @@
+// +build linux,!cgo
+
+package loadavg
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func get() (*Loadavg, error) {
+	file, err := os.Open("/proc/loadavg")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return collectLoadavgStats(file)
+}
+
+func collectLoadavgStats(out io.Reader) (*Loadavg, error) {
+	var loadavgs Loadavg
+	ret, err := fmt.Fscanf(out, "%f %f %f", &loadavgs.Loadavg1, &loadavgs.Loadavg5, &loadavgs.Loadavg15)
+	if err != nil || ret != 3 {
+		return nil, fmt.Errorf("unexpected format of /proc/loadavg")
+	}
+	return &loadavgs, nil
+}
