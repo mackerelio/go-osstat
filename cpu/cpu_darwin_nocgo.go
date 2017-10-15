6caa1f43503357e5e0c0cd23c14fb27a26614222
itchyny <itchyny@hatena.ne.jp>
2017-10-15 11:17:23 +0900

    support cgo/nocgo for darwin

---
+++ cpu/cpu_darwin_nocgo.go
@@ -0,0 +1,57 @@
+// +build darwin,!cgo
+
+package cpu
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os/exec"
+)
+
+// Get cpu statistics
+func Get() (*Cpu, error) {
+	cmd := exec.Command("iostat", "-n0", "-c2")
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+	cpu, err := collectCpuStats(out)
+	if err != nil {
+		return nil, err
+	}
+	if err := cmd.Wait(); err != nil {
+		return nil, err
+	}
+	return cpu, nil
+}
+
+// Cpu represents cpu statistics for darwin
+type Cpu struct {
+	User, System, Idle, Total float64
+}
+
+func collectCpuStats(out io.Reader) (*Cpu, error) {
+	scanner := bufio.NewScanner(out)
+	for i := 0; i < 4; i++ {
+		if !scanner.Scan() {
+			return nil, fmt.Errorf("failed to scan output of iostat")
+		}
+	}
+
+	var cpu Cpu
+	line := scanner.Text()
+	ret, err := fmt.Sscanf(line, "%f %f %f", &cpu.User, &cpu.System, &cpu.Idle)
+	if err != nil || ret != 3 {
+		return nil, fmt.Errorf("unexpected output of iostat")
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scan error for iostat: %s", err)
+	}
+
+	cpu.Total = cpu.User + cpu.System + cpu.Idle
+	return &cpu, nil
+}
