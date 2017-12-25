33d6d5a94783e74139b6dadb9232583174684c51
itchyny <itchyny@hatena.ne.jp>
2017-12-25 12:59:34 +0900

    add tests for swap statistics on freebsd

--- memory/memory_freebsd_test.go
+++ memory/memory_freebsd_test.go
@@ -3,6 +3,7 @@
 package memory
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -15,4 +16,22 @@ func TestGetMemory(t *testing.T) {
 		t.Errorf("invalid memory value: %+v", memory)
 	}
 	t.Logf("memory value: %+v", memory)
+}
+
+func TestCollectSwapStats(t *testing.T) {
+	total, used, err := collectSwapStats(strings.NewReader(
+		`Device          1K-blocks     Used    Avail Capacity
+/dev/gpt/swapfs   1048576    16056  1032520     2%
+`))
+	if err != nil {
+		t.Fatalf("error should be nil but got: %v", err)
+	}
+
+	totalExpected, usedExpected := uint64(1048576)*1024, uint64(16056)*1024
+	if total != totalExpected {
+		t.Errorf("invalid swap total: %+v (expected: %+v)", total, totalExpected)
+	}
+	if used != usedExpected {
+		t.Errorf("invalid swap used: %+v (expected: %+v)", used, usedExpected)
+	}
 }
