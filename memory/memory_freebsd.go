9533611363faff806f4373fe9983f1ee7ee8c738
itchyny <itchyny@hatena.ne.jp>
2017-12-25 12:59:17 +0900

    use swapinfo to retrieve memory swap statistics on freebsd

--- memory/memory_freebsd.go
+++ memory/memory_freebsd.go
@@ -3,7 +3,12 @@
 package memory
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os/exec"
+	"strconv"
+	"strings"
 	"unsafe"
 
 	"golang.org/x/sys/unix"
@@ -11,16 +16,7 @@ import (
 
 // Get memory statistics
 func Get() (*Stats, error) {
-	memory, err := collectMemoryStats()
-	if err != nil {
-		return nil, err
-	}
-
-	// Maybe this is incorrect... needs more research on memory statistics...
-	memory.Used = memory.Total - memory.Free - memory.Cached - memory.Inactive
-	memory.SwapUsed = memory.SwapTotal - memory.SwapFree
-
-	return memory, nil
+	return collectMemoryStats()
 }
 
 // Stats represents memory statistics for freebsd
@@ -48,9 +44,7 @@ func collectMemoryStats() (*Stats, error) {
 		{"vm.stats.vm.v_active_count", &memory.Active, &pageSize},
 		{"vm.stats.vm.v_inactive_count", &memory.Inactive, &pageSize},
 		{"vm.stats.vm.v_wire_count", &memory.Wired, &pageSize},
-		{"vm.swap_total", &memory.SwapTotal, &one},
 	}
-	// TODO: swap_free
 
 	for _, stat := range memStats {
 		ret, err := unix.SysctlRaw(stat.name)
@@ -66,5 +60,50 @@ func collectMemoryStats() (*Stats, error) {
 		}
 	}
 
+	// collect swap statistics from swapinfo command
+	cmd := exec.Command("swapinfo", "-k")
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+	memory.SwapTotal, memory.SwapUsed, err = collectSwapStats(out)
+	if err != nil {
+		return nil, err
+	}
+
+	memory.Used = memory.Total - memory.Free - memory.Cached - memory.Inactive
+	memory.SwapFree = memory.SwapTotal - memory.SwapUsed
+
 	return &memory, nil
+}
+
+func collectSwapStats(out io.Reader) (uint64, uint64, error) {
+	scanner := bufio.NewScanner(out)
+	if !scanner.Scan() {
+		return 0, 0, fmt.Errorf("failed to scan output of swapinfo")
+	}
+	line := scanner.Text()
+	if !strings.HasPrefix(line, "Device") {
+		return 0, 0, fmt.Errorf("unexpected output of swapinfo: %s", line)
+	}
+
+	var total, used uint64
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+		if len(fields) < 5 {
+			continue
+		}
+		if v, err := strconv.ParseUint(fields[1], 10, 64); err == nil {
+			total += v * 1024
+		}
+		if v, err := strconv.ParseUint(fields[2], 10, 64); err == nil {
+			used += v * 1024
+		}
+	}
+
+	return total, used, nil
 }
